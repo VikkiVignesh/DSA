48f4374ca6a3da2b234f1d16744c62e64b87855f
VikkiVignesh <Vikki.Vishnu04@gmail.com>
2024-02-20 07:51:43 +0530

    solved Spiral Traversal

--- Problems/SprialTraversal.java
+++ Problems/SprialTraversal.java
@@ -1,8 +1,10 @@
 import java.util.*;
 public class SprialTraversal {
     public static void main(String[] args) {
-        int arr[][]={ {1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        int arr[][]={ {1,2,3,4,0},{5,6,0,7,8},{0,9,10,11,12},{13,0,14,15,16}};
         display(arr);
+        System.out.println("Spiral Traversal ");
+        sprialtravrse(4, 5, arr);
         
     }
     static void display(int a[][])
@@ -17,15 +19,46 @@ public class SprialTraversal {
         }
     }
 
-    static void sprialtravrse(int a[][])
+    static void sprialtravrse(int row,int col,int a[][])
     {
-        for(int i=0;i<a.length;i++)
+        int i,r=0,c=0;
+        while(r<row && c<col)
         {
-            for(int j=0;j<a[i].length;j++)
+            //printing 1st row
+            for(i=c;i<col;i++)
             {
-                System.out.print(a[i][j]+" ");
+                System.out.print(a[r][i]+" ");
             }
-            
+            r++;
+
+            //printing last column
+            for(i=r;i<row;i++)
+            {
+                System.out.print(a[i][col-1]+" ");
+            }
+            col--;
+
+            //printing last row elements
+            if(r<row)
+            {
+                for(i=col-1;i>=c;i--)
+                {
+                    System.out.print(a[row-1][i]+" ");
+                }
+                row--;
+            }
+
+            //printing 1st column element
+            if(c<col)
+            {
+                for(i=row-1;i>=r;i--)
+                {
+                    System.out.print(a[i][c]+" ");
+                }
+                c++;
+            }
+            System.out.println();
         }
+        
     }
 }
