0b77b4a31592cf8affc6b0300f2b8f862e9439ef
VikkiVignesh <Vikki.Vishnu04@gmail.com>
2024-02-26 19:28:19 +0530

    NumberPyramid  pattern solved

---
+++ Patterns/NumberPyramid.java
@@ -0,0 +1,20 @@
+public class NumberPyramid {
+    public static void main(String[] args) {
+        numpyr(5);
+    }
+    static void numpyr(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++)
+            {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
