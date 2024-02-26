0aa5a1f86c194d926d25cd40f42f86096852c239
VikkiVignesh <Vikki.Vishnu04@gmail.com>
2024-02-26 19:34:04 +0530

    PalindromePyramid  pattern solved

--- Patterns/Palindrome.java
+++ Patterns/Palindrome.java
@@ -1,3 +1,27 @@
 public class Palindrome {
-    
+ 
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    static void pattern(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j=i;j>=1;j--)
+            {
+                System.out.print(j);
+            }
+            for(int k=2;k<=i;k++)
+            {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
 }
