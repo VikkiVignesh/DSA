7ae937484c6f6c1597a8f92b2d68005309548428
VikkiVignesh <Vikki.Vishnu04@gmail.com>
2024-02-26 19:05:02 +0530

    rhombus pattern solved

---
+++ Patterns/solidRhombus.java
@@ -0,0 +1,22 @@
+public class solidRhombus {
+
+    public static void main(String[] args) {
+        rhombus(10);
+    }
+
+    static void rhombus(int n)
+    {
+        for(int  i=0;i<n;i++)
+        {
+            for(int j=0;j< n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=0;k<=2*n;k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
