91ca508f6d596ffdba9c2996263c025de265c6db
VikkiVignesh <Vikki.Vishnu04@gmail.com>
2024-06-17 21:00:36 +0530

    Duplicates in Array

---
+++ Arrays/CheckDuplicates.java
@@ -0,0 +1,37 @@
+public class CheckDuplicates {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5,6};
+        int b[]={1,1,2,3,5,5};
+        System.out.println(checkdup(a));
+        System.out.println(checkdup(b));
+        display(a);
+        display(b);
+    }
+    public static boolean checkdup(int a[])
+    {
+        for(int i=0;i<a.length-1;i++)
+        {
+         for(int j=i+1;j<a.length;j++)
+         {
+            if(a[i]==a[j])
+             return true;
+         }
+        }
+        return false;
+    }
+
+    public static void display(int a[])
+    {
+        System.out.println("Printing Duplicates");
+        for(int i=0;i<a.length-1;i++)
+        {
+            for(int j=i+1;j<a.length;j++)
+            {
+                if(a[i]==a[j])
+                {
+                    System.out.println(a[i]);
+                }
+            }
+        }
+    }
+}
--- Arrays/Stocks.java
+++ Arrays/Stocks.java
@@ -1,8 +1,36 @@
 /*Buy and Sell Stocks
   You are given a Array prices where prices[i] is the price of stock in ith day.You want to maximise the profit by buying one stock a day and selling the stock on some other day in feature.Return teh maximum profit you can achieve a from this transaction.If making profit is nor possible then return 0; 
 */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Stocks {
     public static void main(String[] args) {
-        
+        System.out.println("Welcome to NSE....");
+        System.out.println("Enter the Number of Stocks ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int stock[]=new int[n];
+        System.out.println("Enter Stocks Price");
+        for (int i=0;i<n;i++) {
+          stock[i]=sc.nextInt();
+        }
+        System.out.println("Stock Prices : "+Arrays.toString(stock));
+        System.out.println("Maximum Profit Earned In NSE "+getprofit(stock));
+    }
+
+    public static int getprofit(int a[])
+    {
+      int Profit=0;
+      int CP=a[0];
+      for(int i=1;i<a.length;i++)
+      {
+        CP=Math.min(CP, a[i]);
+        if(a[i]-CP< 0)
+         continue;
+        Profit=Math.max(Profit,a[i]-CP);
+      }
+      return Profit;
     }
 }
