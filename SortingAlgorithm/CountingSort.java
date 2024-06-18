98fa04d43843e276cba7fac72626a883bdc37fa5
VikkiVignesh <Vikki.Vishnu04@gmail.com>
2024-06-18 20:28:51 +0530

    Counting Sort Done

--- SortingAlgorithm/CountingSort.java
+++ SortingAlgorithm/CountingSort.java
@@ -1,5 +1,7 @@
+package SortingAlgorithm;
+import java.util.Arrays;
 import java.util.Scanner;
-import Arrays.
+// import _Arrays.Frequency;
 public class CountingSort {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
@@ -11,6 +13,46 @@ public class CountingSort {
         {
             arr[i]=sc.nextInt();
         }
+
+        System.out.println(Arrays.toString(arr));
+        // Frequency fq=new Frequency();
+        int Freq[]=getFreq(arr);
+        System.out.println(Arrays.toString(Freq));
+
+        cntsort(arr, Freq);
+        System.out.println(Arrays.toString(arr));
     }
-    
+    public static int [] getFreq(int a[])
+    {
+        int max=getmax(a);
+        int temp[]=new int[max+1];
+        for(int i=0;i<a.length;i++)
+        {
+            temp[a[i]]++;
+        }
+        return temp;
+    }
+    public static int getmax(int a[])
+    {
+        int max=a[0];
+        for(int i=1;i<a.length;i++)
+        {
+            if(a[i]>max)
+             max=a[i];
+        }
+        return max;
+    }
+
+    public static void cntsort(int a[],int freq[])
+    {
+        int k=0;
+        for(int i=0;i<freq.length;i++)
+        {
+          while(freq[i]>0)
+          {
+            a[k++]=i;
+            freq[i]--;
+          }
+        }
+    }
 }
