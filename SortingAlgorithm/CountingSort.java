6e0a252e9fafd4aa19e6e8c69c1e1140ddd7385a
VikkiVignesh <Vikki.Vishnu04@gmail.com>
2024-06-18 20:07:30 +0530

    Test

---
+++ SortingAlgorithm/CountingSort.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import Arrays.
+public class CountingSort {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter size of Array");
+        int n=sc.nextInt();
+        System.out.println("Enter Array Elements ");
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+    }
+    
+}
